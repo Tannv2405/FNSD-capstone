6f6c96a255733a9a3cdd831ce2e0cf3f927d472f
Nguyen Tan <tannv240595@gmail.com>
2024-04-04 07:40:39 +0700

    add cicd

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
 
+eksctl create cluster --name simple-jwt-api
+aws sts get-caller-identity --query Account --output text
+aws iam create-role --role-name FlaskDeployCBKubectlRole --assume-role-policy-document file://trust.json --output text --query 'Role.Arn'
+aws iam put-role-policy --role-name FlaskDeployCBKubectlRole --policy-name eks-describe --policy-document file://iam-role-policy.json
+
+kubectl get -n kube-system configmap/aws-auth -o yaml > /tmp/aws-auth-patch.yml
+
+kubectl patch configmap/aws-auth -n kube-system --patch "$(cat /tmp/aws-auth-patch.yml)"
+
+aws ssm put-parameter --name JWT_SECRET --overwrite --value "YourJWTSecret" --type SecureString
